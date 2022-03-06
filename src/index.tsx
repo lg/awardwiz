f6e8c26a84ca22c9e61e93b0fe93994f755f9880
Larry Gadea <trivex@gmail.com>
2022-03-06 12:52:34 -0800

    you can type into a textbox now

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,32 @@ import * as React from "react"
 import * as ReactDOM from "react-dom"
 import "./index.css"
 
+class NearbyAirports extends React.Component<{}, {airportCode: string}> {
+  state = { airportCode: "SFO" }
+
+  render() {
+    return (
+      <div>
+        Airport:
+        <input
+          type="text"
+          value={this.state.airportCode}
+          onChange={(event) => this.setState({ airportCode: event.target.value })}
+        />
+      </div>
+    )
+  }
+}
+
 interface AppState {
 }
 class App extends React.Component<{}, AppState> {
   state: AppState = {}
 
   render() {
-    return <div>hello!</div>
+    return (
+      <NearbyAirports />
+    )
   }
 }
 
