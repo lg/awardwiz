c845e5532df7b9eb634b2ad3c1538cb92ab679dc
Larry Gadea <trivex@gmail.com>
2022-07-09 11:42:12 -0700

    removed mapping for now

--- src/components/SelectAirport.tsx
+++ src/components/SelectAirport.tsx
@@ -1,21 +1,11 @@
 import * as React from "react"
-import { Divider, Select, Tag } from "antd"
-import { ReactElement } from "react"
-import { AirportMap } from "./AirportMap"
+import { Select, Tag } from "antd"
 import { useAirportsDb } from "../hooks/useAirportsDb"
 
 const SelectAirportTag = ({ ...props }) => <Tag style={{ marginRight: 3 }} {...props}>{props.value}</Tag>
 
 export const SelectAirport = ({ ...props }) => {
   const airportsDb = useAirportsDb()
-
-  const menu = (menuElement: ReactElement) => (
-    <>
-      {menuElement}
-      <Divider style={{ margin: "8px 0" }} />
-      <AirportMap airports={props.value} />
-    </>
-  )
 
   return (
     <>
@@ -25,7 +15,6 @@ export const SelectAirport = ({ ...props }) => {
         tokenSeparators={[",", " ", "/"]}
         options={airportsDb.options}
         optionFilterProp="value"
-        dropdownRender={menu}
         {...props}
       />
     </>
