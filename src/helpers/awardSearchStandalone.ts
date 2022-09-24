1f87816df4acb455f13e9b6c4e03043f17e307c6
Larry Gadea <trivex@gmail.com>
2022-09-24 11:50:39 -0700

    worker now runs marked fares

---
+++ src/helpers/awardSearchStandalone.ts
@@ -0,0 +1,19 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { GlobalRegistrator } from "@happy-dom/global-registrator"
+import { renderHook } from "@testing-library/react"
+import { useAwardSearch } from "../hooks/useAwardSearch"
+import { SearchQuery } from "../types/scrapers"
+import pWaitFor from "p-wait-for"
+import { ReactElement } from "react"
+
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+if (!global.window)
+  GlobalRegistrator.register()
+
+export const search = async (searchQuery: SearchQuery, queryClient: QueryClient) => {
+  const wrapper = ({ children }: { children: ReactElement }) => QueryClientProvider({ client: queryClient, children })
+  const { result } = renderHook(() => useAwardSearch(searchQuery), { wrapper })
+  await pWaitFor(() => result.current.loadingQueriesKeys.length === 0, { timeout: { milliseconds: 30000, fallback: () => result.current.stop() } })
+
+  return result.current
+}
--- src/workers/marked-fares.tsx
+++ src/workers/marked-fares.tsx
@@ -2,7 +2,10 @@ import { createClient } from "@supabase/supabase-js"
 import type { Database } from "../types/supabase-types"
 import { MarkedFare } from "../components/SearchResults"
 import dayjs from "dayjs"
+import { search } from "../helpers/awardSearchStandalone"
+import { Listr } from "listr2"
 import { runListrTask } from "../helpers/common"
+import { QueryClient } from "@tanstack/react-query"
 
 const supabase = createClient<Database>(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_SERVICE_KEY)
 
@@ -33,6 +36,22 @@ let markedFares = markedFaresQuery.data!.flatMap((item) => {
 const toRemove = markedFares.filter((markedFare) => dayjs(markedFare.date).isBefore(dayjs().startOf("day")))
 markedFares = markedFares.filter((markedFare) => !toRemove.includes(markedFare))
 
-console.log(markedFares)
+const qc = new QueryClient({ defaultOptions: { queries: { staleTime: Infinity, cacheTime: Infinity, retry: false } } })
+await new Listr<{}>(
+  markedFares.map((markedFare) => ({
+    title: `Querying ${markedFare.origin} to ${markedFare.destination} on ${markedFare.date} for ${markedFare.email}`,
+    task: async (ctx, taskObj) => {
+      const results = await search({ origins: [markedFare.origin], destinations: [markedFare.destination], departureDate: markedFare.date }, qc)
+      const goodResult = results.searchResults.find((result) =>
+        result.flightNo === markedFare.checkFlightNo
+        && result.fares.find((fare) => fare.cabin === markedFare.checkCabin && fare.isSaverFare))
 
-// TODO: check if fares are available
+      // eslint-disable-next-line no-param-reassign
+      taskObj.title = `${taskObj.title} ${goodResult ? "🎉" : ""}`
+    },
+    retry: 3,
+  })), { concurrent: 1, exitOnError: false, registerSignalListeners: false, rendererOptions: { collapseErrors: false } }
+).run()
+
+qc.clear()  // Needed to not have reactquery stall the process from quitting due to cache times
+console.log("done")
