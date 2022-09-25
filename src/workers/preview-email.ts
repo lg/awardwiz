85efcea2bb034daea10477aad6ea92a2cff42b1a
Larry Gadea <trivex@gmail.com>
2022-09-25 01:03:49 -0700

    added email previewing

---
+++ src/workers/preview-email.ts
@@ -0,0 +1,35 @@
+import nodemailer from "nodemailer"
+import handlebars from "handlebars"
+import notificationEmail from "../../emails/notification.html?raw"
+
+console.log("creating test account")
+const testAccount = await nodemailer.createTestAccount()  // use this one for testing
+
+console.log("verifying transport")
+//const transporter = nodemailer.createTransport(import.meta.env.VITE_SMTP_CONNECTION_STRING)   // use this one for production
+const transporter = nodemailer.createTransport(`${testAccount.smtp.secure ? "smtps" : "smtp"}://${testAccount.user}:${testAccount.pass}@${testAccount.smtp.host}:${testAccount.smtp.port}`)
+await transporter.verify()
+
+console.log("compiling template")
+const template = handlebars.compile(notificationEmail)
+
+console.log("sending email")
+const info = await transporter.sendMail({
+  from: import.meta.env.VITE_SMTP_FROM_ADDRESS,
+  to: "trivex@gmail.com",
+  subject: "AwardWiz Notification",
+  priority: "high",
+  html: template({
+    origin: "SFO",
+    destination: "LIH",
+    date: "Dec 10, 2022"
+  }),
+  attachments: [{
+    filename: "wizard.png",
+    path: "src/wizard.png",
+    cid: "wizard.png",
+  }]
+})
+
+console.log("Message sent: %s", info.messageId)
+console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info))
