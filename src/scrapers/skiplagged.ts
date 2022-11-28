32709c3d9f552903586dae10b71b856c939139a8
Larry Gadea <trivex@gmail.com>
2022-11-27 19:27:09 -1000

    hitting the skiplagged api direct now

--- src/scrapers/skiplagged.ts
+++ src/scrapers/skiplagged.ts
@@ -1,33 +1,16 @@
 // Note this scraper doesnt support business class
-// This scraper uses SkipLagged for prices
 
-import { HTTPResponse } from "puppeteer"
 import type { FlightFare, FlightWithFares } from "../types/scrapers"
-import { browserlessInit, BrowserlessInput, gotoPageAndWaitForResponse, Scraper, ScraperMetadata, pptrFetch, log } from "./common"
+import { browserlessInit, BrowserlessInput, Scraper, ScraperMetadata, gotoPage } from "./common"
 import type { SkipLaggedResponse, Segment } from "./samples/skiplagged"
 
 const meta: ScraperMetadata = {
-  name: "skiplagged",
-  blockUrls: ["www.gstatic.com", "/img/", "api/user_info.php", "flex.php"],
+  name: "skiplagged"
 }
 
 export const scraper: Scraper = async (page, query) => {
-  const response = await gotoPageAndWaitForResponse({ page,
-    url: `https://skiplagged.com/flights/${query.origin}/${query.destination}/${query.departureDate}`,
-    waitForResponse: (checkResponse: HTTPResponse) => checkResponse.url().startsWith("https://skiplagged.com/api/search.php")
-  })
-
-  let json: SkipLaggedResponse = await response.json()
-
-  // There's a bug on skiplagged where if you search for a same-day flight, it can sometimes show you flights
-  // a week in the future. In this case, force an xhr to the endpoint with the right date.
-  let receivedDate
-  try { receivedDate = json.flights[Object.keys(json.flights)[0]].segments[0].departure.time.split("T")[0] ?? query.departureDate } catch { receivedDate = query.departureDate }
-  if (receivedDate !== query.departureDate) {
-    log("Skiplagged returned the wrong date's flights, forcing xhr with proper date")
-    const rawResponseForProperDate = await pptrFetch(page, `https://skiplagged.com/api/search.php?from=${query.origin}&to=${query.destination}&depart=${query.departureDate}&return=&format=v3&counts%5Badults%5D=1&counts%5Bchildren%5D=0`, {})
-    json = JSON.parse(rawResponseForProperDate)
-  }
+  const response = await gotoPage(page, `https://skiplagged.com/api/search.php?from=${query.origin}&to=${query.destination}&depart=${query.departureDate}&return=&format=v3&counts%5Badults%5D=1&counts%5Bchildren%5D=0`)
+  const json = await response!.json() as SkipLaggedResponse
 
   const flightsWithFares: FlightWithFares[] = Object.entries(json.flights).map(([id, flight]) => {
     if (flight.count !== 1 || flight.segments.length !== 1)
