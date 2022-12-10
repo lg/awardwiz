14f791b042620eeaf86ba9e37588c3aee7357f6c
Larry Gadea <trivex@gmail.com>
2022-12-09 23:31:38 -0800

    cleaned up firebase functions

--- src/helpers/firebase.ts
+++ src/helpers/firebase.ts
@@ -1,14 +1,17 @@
 import { initializeApp } from "firebase/app"
 import { connectAuthEmulator, getAuth } from "firebase/auth"
 import { connectFirestoreEmulator, getFirestore } from "firebase/firestore"
+import { connectFunctionsEmulator, getFunctions } from "firebase/functions"
 
 // Initialize Firebase
 if (!Object.keys(import.meta.env).includes("VITE_FIREBASE_CONFIG_JSON")) throw new Error("Missing VITE_FIREBASE_CONFIG_JSON environment variable")
 export const firebaseApp = initializeApp(JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG_JSON))
 export const firebaseAuth = getAuth(firebaseApp)
 export const firestore = getFirestore(firebaseApp)
+export const firebaseFunctions = getFunctions(firebaseApp)
 
 if (import.meta.env.VITE_USE_FIREBASE_EMULATORS === "true") {
   connectAuthEmulator(firebaseAuth, "http://localhost:9099")
   connectFirestoreEmulator(firestore, "localhost", 8080)
+  connectFunctionsEmulator(firebaseFunctions, "localhost", 5001)
 }
