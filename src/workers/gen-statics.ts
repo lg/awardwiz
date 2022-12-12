7dd453a5a04596bbe66e81e36a7b83c75f7772e6
Larry Gadea <trivex@gmail.com>
2022-12-11 19:31:51 -1000

    fixed generating statics for real this time

--- src/workers/gen-statics.ts
+++ src/workers/gen-statics.ts
@@ -1,17 +1,16 @@
-/// <reference lib="dom" />
-
 // This script downloads the latest airports and filters/formats them for use with AwardWiz.
 // Use the Launch profile in VSCode to debug this if necessary.
 
 import { Airport } from "../types/scrapers"
 import { find as findTz } from "geo-tz"
 import { writeFileSync } from "fs"
+import fetch from "node-fetch"
 
 const startTime = Date.now()
 
 type FR24Airport = [icaoCode: string, iataCode: string, airportName: string, latitude: number, longitude: number, website: string | undefined, popularity: number, city: string, country: string]
 type FR24Airports = Record<string, FR24Airport>
-const fr24Airports: FR24Airports = await fetch("https://www.flightradar24.com/airports/list").then((airportsRes) => airportsRes.json())
+const fr24Airports = await fetch("https://www.flightradar24.com/airports/list").then((airportsRes) => airportsRes.json()) as FR24Airports
 
 const airports = Object.values(fr24Airports).map((fr24Airport): Airport | undefined => {
   return fr24Airport[1] && fr24Airport[1].length === 3 && fr24Airport[2] ? {
