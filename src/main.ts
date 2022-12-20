a6141f0b7880d5f8ee23940f0314aedfe7e41d7d
Larry Gadea <trivex@gmail.com>
2022-12-19 21:14:09 -0800

    quick rearrangement

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,11 @@ import { runScraper } from "./scraper.js"
 import { AwardWizScraperModule, AwardWizQuery } from "./types.js"
 
 for (let i: number = 0; i < 5; i += 1) {
+  const scraper: AwardWizScraperModule = await import("./scrapers/aa.js")
+  const query: AwardWizQuery = { origin: "JFK", destination: "SFO", departureDate: "2022-12-20" }
+
   await runScraper(async (sc) => {
-    const scraper: AwardWizScraperModule = await import("./scrapers/aa.js")
-    const query: AwardWizQuery = { origin: "JFK", destination: "SFO", departureDate: "2022-12-20" }
     log(sc, "Using query:", query)
     return scraper.runScraper(sc, query)
-  }, { name: "test" })
+  }, { name: scraper.meta.name })
 }
