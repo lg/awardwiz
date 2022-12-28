e8dfea5010243ea3201e5c1d77b5ed0bd8bb5292
Larry Gadea <trivex@gmail.com>
2022-12-27 16:31:45 -1000

    fixed stats

--- src/stats.ts
+++ src/stats.ts
@@ -4,13 +4,17 @@ export const enableStatsForContext = (context: BrowserContext) => {
   let totCacheHits = 0, totCacheMisses = 0, bytesDownloaded = 0, totBlocked = 0
   const domains: Record<string, number> = {}
 
-  context.on("response", async response => {
-    if (await response.headerValue("x-fromcache")) {
+  context.on("requestfinished", async (req) => {
+    const response = await req.response()
+    if (response?.headers()["x-fromcache"]) {
       totCacheHits += 1
     } else {
       totCacheMisses += 1
-      const hostname = new URL(response.url()).hostname
-      const bytes = parseInt(await response.headerValue("content-length") ?? (await response.body().catch(e => Buffer.from(""))).byteLength.toString())
+
+      const hostname = new URL(req.url()).hostname
+      const sizes = await req.sizes()
+      const bytes = sizes.responseBodySize + sizes.responseHeadersSize
+
       domains[hostname] = (domains[hostname] || 0) + bytes
       bytesDownloaded += bytes
     }
