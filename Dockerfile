c205f2c5cdb5d62d30079819c4c95f502d0e3d82
Larry Gadea <trivex@gmail.com>
2023-01-06 20:01:34 -0800

    dockerfile's debug statements are now optional

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,18 @@
 # tags here: https://mcr.microsoft.com/en-us/product/playwright/tags
 FROM mcr.microsoft.com/playwright:v1.29.1
 WORKDIR /root
+ARG DEBUG=0
 
 # tools to debug via vnc (connect to this instance on port 8282)
 ENV DISPLAY=:0.0
-RUN apt update && DEBIAN_FRONTEND="noninteractive" TZ="America/Los_Angeles" apt install -y x11vnc novnc xvfb fluxbox
+RUN apt update && DEBIAN_FRONTEND="noninteractive" TZ="America/Los_Angeles" apt install -y xvfb
+RUN if [ $DEBUG = 1 ]; then apt install -y x11vnc novnc fluxbox; fi
 EXPOSE 8282
 
 # redis is used for caching urls
-RUN curl -fsSL https://packages.redis.io/gpg | gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg \
+RUN if [ $DEBUG = 1 ]; then curl -fsSL https://packages.redis.io/gpg | gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg \
   && echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/redis.list \
-  && apt update && apt install -y redis
+  && apt update && apt install -y redis; fi
 
 # code should already be available in dist/ as javascript
 ENV NODE_ENV production
--- run.sh
+++ run.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 set -eo pipefail
 
-# If this script is started with PORT defined, we're assuming we're in production server mode
-
 if [ -n "${MOVE_BROWSERS_TO_PATH}" ]; then
   cp -r /ms-playwright/* "${MOVE_BROWSERS_TO_PATH}"
   export PLAYWRIGHT_BROWSERS_PATH="${MOVE_BROWSERS_TO_PATH}"
   rm -rf "/ms-playwright"
 fi
 
-# Start a web server on port 8282 so we can view the browser
 Xvfb :0 -screen 0 1280x900x16 -listen tcp -ac &
-if [ -z "${PORT}" ]; then
+
+# Start a web server on port 8282 so we can view the browser
+if which x11vnc > /dev/null; then
   x11vnc -forever -shared -nopw -quiet 2>/dev/null >/dev/null &
   websockify --daemon --web /usr/share/novnc 8282 localhost:5900 2>/dev/null
   fluxbox 2>/dev/null &
@@ -19,13 +18,16 @@ fi
 
 mkdir -p ./tmp
 if [ -z "${REDIS_URL}" ]; then
-  echo -e "maxmemory 128mb\\n daemonize yes\\n dir ./tmp" | redis-server -
+  if which redis-server > /dev/null; then
+    echo -e "maxmemory 128mb\\n daemonize yes\\n dir ./tmp" | redis-server -
+  fi
 fi
 
-if [ -z "${PORT}" ]; then
-  node --enable-source-maps --unhandled-rejections=strict --trace-uncaught --trace-warnings dist/main-debug.js
-else
+if [ -n "${PORT}" ]; then
   node --enable-source-maps --unhandled-rejections=strict --trace-uncaught --trace-warnings dist/main-server.js
+else
+  echo "* debug mode *"
+  node --enable-source-maps --unhandled-rejections=strict --trace-uncaught --trace-warnings dist/main-debug.js
 fi
 
 exec redis-cli shutdown
