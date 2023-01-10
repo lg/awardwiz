5b62db62afe01fe9dda5f6b4a285e8d497c91582
Larry Gadea <trivex@gmail.com>
2023-01-09 23:11:21 -0800

    upped test concurrency since our backend is a lot faster now

--- vite.config.ts
+++ vite.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
 
   test: {
     globals: true,
-    maxConcurrency: 3,
+    maxConcurrency: 5,
     testTimeout: 3 * 60000, // incase we get in the test queue on browserless
     environment: "jsdom",
     onConsoleLog: (log, type) => {
