ac0d082190b3b88aeac4c3c02f86a7488154af65
Larry Gadea <trivex@gmail.com>
2023-04-09 23:44:46 -0700

    this is why we can't have nice things

--- src/helpers/runScraper.ts
+++ src/helpers/runScraper.ts
@@ -1,7 +1,14 @@
 import axios, { GenericAbortSignal } from "axios"
 import { DatedRoute } from "../hooks/useAwardSearch"
 import { ScraperResponse } from "../types/scrapers"
+import { firebaseAuth } from "../helpers/firebase"
 
 export const runScraper = async <T = ScraperResponse>(scraperName: string, datedRoute: DatedRoute, signal: GenericAbortSignal | undefined) => {
-  return axios.get<T>(`${import.meta.env.VITE_SCRAPERS_URL}/run/${scraperName}-${datedRoute.origin}-${datedRoute.destination}-${datedRoute.departureDate}`, { signal })
+  const token = await firebaseAuth.currentUser?.getIdToken()
+  return axios.get<T>(`${import.meta.env.VITE_SCRAPERS_URL}/run/${scraperName}-${datedRoute.origin}-${datedRoute.destination}-${datedRoute.departureDate}`, {
+    headers: {
+      Authorization: `Bearer ${token}`
+    },
+    signal
+  })
 }
