893ffe70e0aba2fbf91382d19e1c279953cc6d59
Larry Gadea <trivex@gmail.com>
2023-04-23 15:59:33 -0700

    added COACH class to alaska

--- awardwiz-scrapers/scrapers/alaska.ts
+++ awardwiz-scrapers/scrapers/alaska.ts
@@ -32,7 +32,6 @@ export const runScraper: AwardWizScraper = async (arkalis, query) => {
 
 const standardizeResults = (raw: AlaskaResponse, query: AwardWizQuery): FlightWithFares[] => {
   const results: FlightWithFares[] = []
-  debugger
   for (const slice of raw.slices!) {
     if (slice.segments.length > 1)
       continue
@@ -61,12 +60,12 @@ const standardizeResults = (raw: AlaskaResponse, query: AwardWizQuery): FlightWi
         throw new Error(`multiple booking codes\n${JSON.stringify(fare, null, 2)}}`)
       if (fare.cabins.length !== 1)
         throw new Error(`multiple cabins\n${JSON.stringify(fare, null, 2)}}`)
-      if (fare.cabins[0] !== "MAIN" && fare.cabins[0] !== "FIRST" && fare.cabins[0] !== "SAVER")
+      if (fare.cabins[0] !== "MAIN" && fare.cabins[0] !== "FIRST" && fare.cabins[0] !== "SAVER" && fare.cabins[0] !== "COACH")
         throw new Error(`unknown cabin: ${fare.cabins[0]}\n${JSON.stringify(fare, null, 2)}}`)
 
       const fareToAdd: FlightFare = {
         bookingClass: fare.bookingCodes[0],
-        cabin: {"FIRST": "business", "MAIN": "economy", "SAVER": "economy"}[fare.cabins[0]!]!,
+        cabin: {"FIRST": "business", "MAIN": "economy", "SAVER": "economy", "COACH": "economy"}[fare.cabins[0]!]!,
         cash: fare.grandTotal,
         currencyOfCash: "USD",
         miles: fare.milesPoints,
