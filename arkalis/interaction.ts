10fec47cf2f0ec59841756800eb9a0e53f149450
Larry Gadea <trivex@gmail.com>
2023-08-17 01:34:07 +0000

    interaction in arkalis should no longer require the window object, cleans up dependencies

--- arkalis/interaction.ts
+++ arkalis/interaction.ts
@@ -1,9 +1,15 @@
-import { Arkalis, ArkalisCore } from "./arkalis.js"
+import { ArkalisCore } from "./arkalis.js"
 import { Bezier, Point } from "bezier-js"
 
 export const arkalisInteraction = (arkalis: ArkalisCore) => {
-  const viewportSize = (arkalis as Arkalis).window.size
-  let curMousePos = [Math.random() * viewportSize[0]!, 0]  // start at top of viewport since that's where the URL bar would be
+  let viewPortSizeMemo: [number, number] | undefined
+  const getViewportSize = async () => viewPortSizeMemo ??= await (async () => {
+    const layoutMetrics = await arkalis.client.Page.getLayoutMetrics()
+    return [layoutMetrics.layoutViewport.clientWidth, layoutMetrics.layoutViewport.clientHeight]
+  })()
+
+  let curMousePosMemo: [number, number] | undefined
+  const getCurMousePos = async () => curMousePosMemo ??= [Math.random() * (await getViewportSize())[0], 0]
 
   async function getBoxForSelector(selector: string) {
     const doc = await arkalis.client.DOM.getDocument({ depth: -1 })
@@ -13,23 +19,25 @@ export const arkalisInteraction = (arkalis: ArkalisCore) => {
     return arkalis.client.DOM.getBoxModel({ nodeId: node.nodeId })
   }
 
-  function genPath(start: number[], end: number[]) {
+  async function genPath(start: [number, number], end: [number, number]) {
     const EXTRA_BEZIER_POINTS = Math.floor(Math.random() * 2) + 1
     const TOTAL_POINTS = 80 + Math.floor(Math.random() * 20)
     const OVERSHOOT = 20 + (Math.random() * 10)
 
-    if (start[0]! > viewportSize[0]! || start[1]! > viewportSize[1]! ||
-        end[0]! > viewportSize[0]! || end[1]! > viewportSize[1]!)
+    const viewportSize = await getViewportSize()
+
+    if (start[0] > viewportSize[0] || start[1] > viewportSize[1] ||
+        end[0] > viewportSize[0] || end[1] > viewportSize[1])
       throw new Error("start or end point of area to click is outside of viewport")
 
     // Get the curve (using general method from https://github.com/Xetera/ghost-cursor)
     const bezier = new Bezier([
-      { x: start[0]!, y: start[1]! },
+      { x: start[0], y: start[1] },
       ...Array(EXTRA_BEZIER_POINTS).fill(0).map(() => ({
-        x: Math.random() * (end[0]! - start[0]!) + start[0]!,
-        y: Math.random() * (end[1]! - start[1]!) + start[1]!
+        x: Math.random() * (end[0] - start[0]) + start[0],
+        y: Math.random() * (end[1] - start[1]) + start[1]
       })),
-      { x: end[0]!, y: end[1]! }
+      { x: end[0], y: end[1] }
     ])
 
     // Apply the easing function as per https://incolumitas.com/2021/05/20/avoid-puppeteer-and-playwright-for-scraping/.
@@ -43,10 +51,11 @@ export const arkalisInteraction = (arkalis: ArkalisCore) => {
   }
 
   async function plotPath(path: Point[]) {
+    const viewportSize = await getViewportSize()
     await arkalis.client.Runtime.evaluate({ expression: `(() => {
       const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-      svg.setAttribute("width", "${viewportSize[0]!}");
-      svg.setAttribute("height", "${viewportSize[1]!}");
+      svg.setAttribute("width", "${viewportSize[0]}");
+      svg.setAttribute("height", "${viewportSize[1]}");
       svg.setAttribute("style", "position: fixed; top: 0; left: 0; z-index: 10000; pointer-events: none;");
       svg.innerHTML = \`${path.map((point, i) => {
         const color = i === path.length - 1 ? "red" : "lightgreen"
@@ -56,8 +65,8 @@ export const arkalisInteraction = (arkalis: ArkalisCore) => {
     })()`})
   }
 
-  async function moveMouseTo(pos: number[]) {
-    const path = genPath(curMousePos, pos)
+  async function moveMouseTo(pos: [number, number]) {
+    const path = await genPath(await getCurMousePos(), pos)
     if (arkalis.debugOptions.drawMousePath)
       void plotPath(path)
 
@@ -66,7 +75,7 @@ export const arkalisInteraction = (arkalis: ArkalisCore) => {
       await arkalis.wait((path[i] as unknown as { t: number }).t - lastTime)
       await arkalis.client.Input.dispatchMouseEvent({ type: "mouseMoved", x: point.x, y: point.y })
     }
-    curMousePos = pos
+    curMousePosMemo = pos
   }
 
   return {
@@ -74,13 +83,14 @@ export const arkalisInteraction = (arkalis: ArkalisCore) => {
       await arkalis.wait(Math.random() * 100)
 
       const box = await getBoxForSelector(selector)
-      const point = [(box.model.content[2]! - box.model.content[0]!) * Math.random() + box.model.content[0]!,
-        (box.model.content[5]! - box.model.content[1]!) * Math.random() + box.model.content[1]!]
+      const boxContent = box.model.content as [number, number, number, number, number, number, number, number]
+      const point = [(boxContent[2] - boxContent[0]) * Math.random() + boxContent[0],
+        (boxContent[5] - boxContent[1]) * Math.random() + boxContent[1]] as [number, number]
       await moveMouseTo(point)
       await arkalis.wait(Math.random() * 100 + 50)
-      await arkalis.client.Input.dispatchMouseEvent({ type: "mousePressed", x: point[0]!, y: point[1]!, button: "left", clickCount: 1 })
+      await arkalis.client.Input.dispatchMouseEvent({ type: "mousePressed", x: point[0], y: point[1], button: "left", clickCount: 1 })
       await arkalis.wait(Math.random() * 100 + 50)
-      await arkalis.client.Input.dispatchMouseEvent({ type: "mouseReleased", x: point[0]!, y: point[1]!, button: "left", clickCount: 1 })
+      await arkalis.client.Input.dispatchMouseEvent({ type: "mouseReleased", x: point[0], y: point[1], button: "left", clickCount: 1 })
     }
   }
 }
